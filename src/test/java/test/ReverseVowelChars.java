4e4e76c67e9ea54298b1ea039f49ac3152bc1e1d
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-04-13 14:56:03 +0530

    comitting reverse Vowels new logic

--- src/test/java/test/ReverseVowelChars.java
+++ src/test/java/test/ReverseVowelChars.java
@@ -6,43 +6,47 @@ import org.testng.annotations.Test;
 public class ReverseVowelChars
 {
     @Test
-    public void test1() {
-        String input = "programming";
-        String output = "prigrammong";
-        Assert.assertEquals(checkReversingVowels(input), output);
+    public void test1()
+    {
+        String inputText = "programming";
+        String outputText = "prigrammong";
+        Assert.assertEquals(reversedString(inputText),outputText);
     }
+
     @Test
-    public void test2() {
-        String input = "global";
-        String output = "glabol";
-        Assert.assertEquals(checkReversingVowels(input), output);
+    public void test2()
+    {
+        String inputText = "fantastic";
+        String outputText = "fintastac";
+        Assert.assertEquals(reversedString(inputText),outputText);
     }
 
-    public String checkReversingVowels(String input) {
-        char[] inputArray = input.toCharArray();
-        int left = 0;
-        int right = inputArray.length - 1;
-        while (left < right) {
-            if (!isVowel(inputArray[left])) {
-                left++;
-            } else if (!isVowel(inputArray[right])) {
-                right--;
-            } else {
-                char temp = inputArray[left];
-                inputArray[left] = inputArray[right];
-                inputArray[right] = temp;
-                left++;
-                right--;
-            }
-        }
-            return new String(inputArray);
-        }
-
-
-        public boolean isVowel ( char ch)
+public String reversedString(String inputText)
+{
+    char[] charArray = inputText.toCharArray();
+    int left = 0;
+    int right = charArray.length - 1;
+    while(left<right)
+    {
+        if(isVowel(charArray[left]) && isVowel(charArray[right]))
         {
-            return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
         }
-
+        else
+        {
+            left++;
+            right--;
+        }
+        }
+    return new String(charArray);
+    }
+    public boolean isVowel(char ch)
+    {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||  ch == 'u';
     }
 
+}
