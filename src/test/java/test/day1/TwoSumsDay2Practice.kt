20a5f3e7cc7c78f945c2252bdff4f78f3efafb47
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-05-22 21:52:57 +0530

    Update TwoSumsDay2Practice.kt

--- src/test/java/test/day1/TwoSumsDay2Practice.kt
+++ src/test/java/test/day1/TwoSumsDay2Practice.kt
@@ -1,5 +1,19 @@
-package test.day1
+package test.day1;
 
-class TwoSumsDay2Practice {
-    //practice the 2 sum problem bruteforce tomorrow may 22
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.Arrays;
+
+public class TwoSumsDay2Practice
+{
+    @Test
+    public void test01()
+    {
+        int[] firstInput = {5,4,6,3,4,5,0};
+        int target = 10;
+        int[] output01 = sumOfTwo(firstInput, target);
+        int[] output = {0,5};
+        Assert.assertTrue(Arrays.equals(output01,output));
+    }
+    
 }
