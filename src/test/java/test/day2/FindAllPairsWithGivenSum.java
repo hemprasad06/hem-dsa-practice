bd116b3fc708bd80124c184e45d2c80cb6b59a49
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-05-27 21:21:34 +0530

    Update FindAllPairsWithGivenSum.java

--- src/test/java/test/day2/FindAllPairsWithGivenSum.java
+++ src/test/java/test/day2/FindAllPairsWithGivenSum.java
@@ -6,7 +6,24 @@ public class FindAllPairsWithGivenSum {
     @Test
     public void test02()
     {
-        
+        int[] input = {0,2,4,6,3,3,1,5};
+        int target = 6;
+        int[] output = findAllPairs(input, target);
+    }
+
+    private int[] findAllPairs(int[] input, int target) {
+        for(int i = 0; i < input.length; i++)
+        {
+            for(int j = i+1; j < input.length; j++)
+            {
+                if(input[i] + input[j] == target)
+                {
+                   //System.out.println("This " + input[i] + " + " + input[j] + " = " + target);
+                    System.out.println("("+input[i]+","+input[j]+")");
+                }
+            }
+        }
+        return null;
     }
 
 }
