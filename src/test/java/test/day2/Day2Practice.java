4b1aa876da9b555d516ca9be7cdc4dcd4cfea5c0
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-06-01 22:15:56 +0530

    Create Day2Practice.java

---
+++ src/test/java/test/day2/Day2Practice.java
@@ -0,0 +1,11 @@
+package test.day2;
+
+public class Day2Practice {
+    @Test
+    public void test1()
+    {
+        int[] input = {0,1,};
+        int target = 7;
+        int expectedResult = day2Practice(input,target);
+    }
+}
