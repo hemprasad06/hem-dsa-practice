3368096b7d98406570d1321a8735391f8611c2eb
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-06-01 22:22:46 +0530

    Update Day2Practice.java

--- src/test/java/test/day2/Day2Practice.java
+++ src/test/java/test/day2/Day2Practice.java
@@ -1,11 +1,42 @@
 package test.day2;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
 public class Day2Practice {
     @Test
     public void test1()
     {
-        int[] input = {0,1,};
-        int target = 7;
+        int[] input = {0,2,4,6,3,3,1,5};
+        int target = 6;
         int expectedResult = day2Practice(input,target);
+        int expectedResult01 = 4;
+        Assert.assertEquals(expectedResult,expectedResult01);
+    }
+    @Test
+    public void test2()
+    {
+        int[] input = {0,7,0,4,3,5,0,2,0};
+        int target = 7;
+        int actualResult02 = day2Practice(input, target);
+        int expectedResult02 = 6;
+        Assert.assertEquals(expectedResult02,actualResult02);
+    }
+
+    public int day2Practice(int[] input, int target)
+    {
+        Map<Integer,Integer> mapped = new Hashmap<>();
+        int count = 0;
+        for(int num: input)
+        {
+            int complement = target - num;
+            if(mapped.containsKey(complement))
+            {
+                count+=mapped.get(complement);
+            }
+        }
+
     }
 }
